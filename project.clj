11a2d5b8580d6dfbd2f69fe20b48040c284221f5
Dave Tenny <dave.tenny@gmail.com>
2014-05-02 06:23:05 -0400

    Prepare 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject jdt "0.1.0-SNAPSHOT"
+(defproject jdt "0.1.0"
   :description "These are not the tools you're looking for."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/dtenny/clojdt"
+  :scm {:name "git"
+        :url "git@github.com:dtenny/clojdt.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [;[org.clojure/clojure "1.6.0-RC1"]
@@ -8,6 +10,6 @@
                  [me.raynes/conch "0.6.0"]
                  [me.raynes/fs "1.4.5"] ; presently a locally managed repo
                  [org.clojure/java.classpath "0.2.2"]
-                 [enlive "1.1.5"] ; net.cgrand.enlive-html - for html parsin (and minor fetching)
+                 [enlive "1.1.5"] ; net.cgrand.enlive-html - for html parsing (and minor fetching)
                  [ch.ethz.ganymed/ganymed-ssh2 "261"]
                  ])
